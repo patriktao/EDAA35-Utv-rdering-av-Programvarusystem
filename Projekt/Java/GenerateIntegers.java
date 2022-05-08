1236909c8e7dad628ca63a611afdebd338290c13
arvid0131 <arvidcarp@hotmail.se>
2022-05-08 21:46:11 +0200

    New GenerateIntegers

--- Projekt/Java/GenerateIntegers.java
+++ Projekt/Java/GenerateIntegers.java
@@ -16,14 +16,37 @@ public class GenerateIntegers {
         int iterations = Integer.parseInt(args[1]);
         try {
             BufferedWriter outputWriter = new BufferedWriter(new FileWriter(output));
-            /*
-             * for (int i = 0; i < iterations; i++) {
-             * int r = getRandomNumber(0, 10000);
-             * outputWriter.append(r + "\n");
-             * }
-             */
-            for (int i = 0; i < iterations; i++) {
-                outputWriter.append(i + "\n");
+            if (args[2] == "random") {
+	            for (int i = 0; i < iterations; i++) {
+	                int r = getRandomNumber(0, 10000);
+	                outputWriter.append(r + "\n");
+	            }
+            }
+            else if (args[2] == "semi-random") {
+            	if (args[3] == "reverse") {
+            		for (int i = iterations; i > 0; i--) {
+            			int r = getSemiRandomNumber(i, iterations);
+    	                outputWriter.append(r + "\n");
+            		}
+            	}
+            	else if (args[3] == "ordered"){
+            		for (int i = 0; i < iterations; i++) {
+            			int r = getSemiRandomNumber(i, iterations);
+    	                outputWriter.append(r + "\n");
+            		}
+            	}
+            }
+            else if (args[2] == "sorted") {
+            	if (args[3] == "reverse") {
+            		for (int i = iterations; i > 0; i--) {
+    	                outputWriter.append(i + "\n");
+            		}
+            	}
+            	else if (args[3] == "ordered"){
+            		for (int i = 0; i < iterations; i++) {
+    	                outputWriter.append(i + "\n");
+            		}
+            	}
             }
             outputWriter.close();
         } catch (IOException e) {
@@ -35,4 +58,15 @@ public class GenerateIntegers {
         return (int) ((Math.random() * (max - min)) + min);
     }
 
+    private int getSemiRandomNumber(int n, int iterations) {
+    	if (n < 50) {
+    		return getRandomNumber(0, n + 50);
+    	}
+    	else if (n > iterations - 50) {
+    		return getRandomNumber(n - 50, iterations);
+    	}
+    	else {
+    		return getRandomNumber(n - 50, n + 50);
+    	}
+    }
 }
