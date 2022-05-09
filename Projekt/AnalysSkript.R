8b5d0abc546cfe77faf3d31c34c9a827563bfd94
Patrik <patriktao@gmail.com>
2022-05-09 20:57:36 +0200

    update

--- Projekt/AnalysSkript.R
+++ Projekt/AnalysSkript.R
@@ -1,9 +1,10 @@
 source("https://fileadmin.cs.lth.se/cs/Education/EDAA35/R_resources.R")
 
+
 createPlots <- function(file, start = 1) {
   data <- read.csv(file)
   data <- data[start:nrow(data),]
-  pdf("ordered_1000.pdf")
+  pdf("data_random_50.pdf")
   par(mfrow=c(2,1))
   plot(data$mergeSort,  type="l", main="MergeSort", xlab="Iterations", ylab="Time (ns)")
   plot(data$quickSort, type="l", main="QuickSort", xlab="Iterations", ylab="Time (ns)")
@@ -17,9 +18,12 @@ meanSortingValues <- function(file, start=1){
   return(mean(data$quickSort))
 }
 
-meanValue <- function(){
+meanValue <- function(counter){
   meanValues <- vector()
-  meanValues <- append(meanValues, meanSortingValues("Java/res_random_50"))
+  for(i in 1:counter){
+    system("java IntegerSort data_ordered_500 res_ordered_500 100")
+    meanValues <- append(meanValues, meanSortingValues("res_ordered_500"))
+  }
   mean <- mean(meanValues)
   median <- median(meanValues)
   cI <- confidenceInterval(meanValues, confidenceLevel=0.95)
